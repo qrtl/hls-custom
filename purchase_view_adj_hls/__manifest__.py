0ad6ed58b81ddb6a690f83670cf3d527822e437c
Tomoko Matsumura <45782518+nakometal@users.noreply.github.com>
2019-08-16 15:27:13 +0900

    #459 Adjustment Purchase Order View (#22)
    * [ADD] purchase_view_adj_hls
    
    * [ADD] replace fields
    
    * [ADD] replace and adjustment fields
    
    * [Add] translate JP
    
    * adjust styling, etc.
    
    * adjust styling

---
+++ purchase_view_adj_hls/__manifest__.py
@@ -0,0 +1,21 @@
+# Copyright 2019 Quartile Limited
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+{
+    "name": "Purchase View Adj HLS",
+    "summary": "",
+    "description": """
+    """,
+    'version': '12.0.1.0.0',
+    "category": "Purchase",
+    "website": "https://www.quartile.co/",
+    "author": "Quartile Limited",
+    "license": "AGPL-3",
+    "application": False,
+    "installable": True,
+    "depends": [
+        "purchase_stock"
+    ],
+    "data": [
+        'views/purchase_order_views.xml',
+    ],
+}
