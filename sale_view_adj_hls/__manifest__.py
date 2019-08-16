be8ec36f0b1f07e64e193a2c710661db0794c7a9
Tomoko Matsumura <45782518+nakometal@users.noreply.github.com>
2019-08-16 14:56:40 +0900

    #459 Adjustment Sale Order View (#21)
    * [ADD] sale_view_adj_hls
    
    * [ADD] invisible fields
    
    * [ADD] header fields
    
    * [FIX] depends
    
    sale -> sales_stock
    https://github.com/qrtl/hls-custom/pull/21#pullrequestreview-275276594
    
    * [FIX] warehouse_id inherit form
    
    https://github.com/qrtl/hls-custom/pull/21#discussion_r314188156
    
    * [FIX] replace field
    
    https://github.com/qrtl/hls-custom/pull/21#discussion_r314188557
    
    * adjust styling
    
    * adjust styling

---
+++ sale_view_adj_hls/__manifest__.py
@@ -0,0 +1,21 @@
+# Copyright 2019 Quartile Limited
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+{
+    "name": "Sale View Adj HLS",
+    "summary": "",
+    "description": """
+    """,
+    'version': '12.0.1.0.0',
+    "category": "Sales",
+    "website": "https://www.quartile.co/",
+    "author": "Quartile Limited",
+    "license": "AGPL-3",
+    "application": False,
+    "installable": True,
+    "depends": [
+        "sale_stock"
+    ],
+    "data": [
+        'views/sale_order_views.xml',
+    ],
+}
