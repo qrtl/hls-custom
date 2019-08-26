11b62e43d6a3cc207653066bad680bc83c819881
Yoshi Tashiro <tashiro@quartile.co>
2019-08-26 15:53:29 +0900

    [UPD] stock_delivery_request_report (#30)
    * WIP
    
    * [REM] Delivery Request Form on Stock Picking
    
    * [ADD] Delivery Request Form button under the print
    
    * [ADD] carrier_id on Stock Picking Batch Form
    
    * [FIX] field_name
    
    * [ADD] domain carrier_id on picking_ids tree
    
    * [WIP] domain carrier_id on picking_ids tree
    
    * [FIX] form id and name
    
    * set search default for picking_ids

---
+++ stock_delivery_request_report/models/stock_picking_batch.py
@@ -0,0 +1,11 @@
+# Copyright 2019 Quartile Limited
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from odoo import models, fields
+
+class StockPickingBatch(models.Model):
+    _inherit = 'stock.picking.batch'
+
+    carrier_id  = fields.Many2one(
+        'delivery.carrier'
+    )
