3c9e15013956b1b02a2aff64b34459b5666c0cce
Yoshi Tashiro <tashiro@quartile.co>
2019-08-27 17:49:30 +0800

    update template, update translation

--- stock_delivery_request_report/wizard/stock_picking_report_wizard.py
+++ stock_delivery_request_report/wizard/stock_picking_report_wizard.py
@@ -9,10 +9,11 @@ class StockPickingReportWizard(models.TransientModel):
 
     def print_delivery_request_form(self):
         context = dict(self.env.context)
-        bps = self.env['stock.picking.batch'].browse(context.get('active_ids'))
-        for bp in bps:
-            report_id = self.env['stock.picking.report'].\
-                _create_delivery_request_form(bp)
-            return self.env.ref(
-                'stock_delivery_request_report.delivery_request_form'
-                ).report_action([report_id])
+        # here we only handle one picking batch record
+        bp = self.env['stock.picking.batch'].browse(
+            context.get('active_ids'))[0]
+        report_id = self.env['stock.picking.report'].\
+            _create_delivery_request_form(bp)
+        return self.env.ref(
+            'stock_delivery_request_report.delivery_request_form'
+            ).report_action([report_id])
