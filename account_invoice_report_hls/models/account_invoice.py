1165cedcd94423a739b1f476de8600d40340c167
Yoshi Tashiro <tashiro@quartile.co>
2019-09-02 16:37:02 +0800

    imp usability of invoice form

--- account_invoice_report_hls/models/account_invoice.py
+++ account_invoice_report_hls/models/account_invoice.py
@@ -5,7 +5,7 @@ from datetime import datetime
 from dateutil.relativedelta import relativedelta
 
 from odoo import fields, models, api, _
-
+from odoo.exceptions import ValidationError
 
 
 class AccountInvoice(models.Model):
@@ -33,7 +33,31 @@ class AccountInvoice(models.Model):
         for inv in self:
             date = inv.date_invoice or fields.Date.from_string(
                 fields.Date.context_today(self))
-            # here we assume that all the invoices will be in Japanese
-            inv.doc_title = str(date.month) + '月分'
             inv.date_from = date.replace(day=1)
             inv.date_to = inv.date_from + relativedelta(months=1, days=-1)
+
+    @api.onchange('date_from')
+    def _onchange_date_from(self):
+        for inv in self:
+            inv.doc_title = str(inv.date_from.month) + '月分' if inv.date_from \
+            else False
+
+    @api.multi
+    def action_date_assign(self):
+        # populate date range at validation in case the fields are not updated
+        # before validating the invoice
+        res = super(AccountInvoice, self).action_date_assign()
+        for inv in self.filtered(lambda x: x.monthly_invoice and (
+                not x.date_from or not x.date_to)):
+            inv._onchange_date_invoice()
+            # respect the already existing doc title.
+            if not inv.doc_title:
+                inv._onchange_date_from()
+        return res
+
+    @api.constrains('date_from', 'date_to')
+    def _check_date_range(self):
+        for inv in self:
+            if inv.date_from and inv.date_to and inv.date_from > inv.date_to:
+                raise ValidationError(_(
+                    "Date To must not be earlier than Date From."))
