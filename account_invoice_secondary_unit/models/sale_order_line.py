9583c5c3ed4905844dd9a3d6bf151168d61fc562
Tomoko Matsumura <tomoko.m.0123@gmail.com>
2019-09-04 13:32:50 +0800

    [FIX] fix eview contents

--- account_invoice_secondary_unit/models/sale_order_line.py
+++ account_invoice_secondary_unit/models/sale_order_line.py
@@ -13,16 +13,17 @@ class SaleOrderLine(models.Model):
         self.ensure_one()
         res = super(SaleOrderLine, self)._prepare_invoice_line(qty)
         if self.secondary_uom_id:
-            res['secondary_uom_qty'] = self._get_secondary_uom_qty()
+            res['secondary_uom_qty'] = self._get_secondary_uom_qty(qty)
             res['secondary_uom_id'] = self.secondary_uom_id.id
         return res
 
     @api.multi
-    def _get_secondary_uom_qty(self):
+    def _get_secondary_uom_qty(self, qty):
+        self.ensure_one()
         factor = self.secondary_uom_id.factor * \
             self.product_uom.factor
         rounding_factor = self.secondary_uom_id.uom_id.factor
         return float_round(
-            self.qty_delivered / (factor or 1.0),
+            qty / (factor or 1.0),
             precision_rounding=rounding_factor
         )
