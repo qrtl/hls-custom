5401e074dfa366a49c14bbfc87ac8ed8a8fb86e0
Tomoko Matsumura <tomoko.m.0123@gmail.com>
2019-09-04 12:59:35 +0800

    [IMP] Recalculation secondary unit quantity
    When creating an invoice,
    Recalculate the secondary unit quantity based on the delivered quantity.

--- account_invoice_secondary_unit/models/sale_order_line.py
+++ account_invoice_secondary_unit/models/sale_order_line.py
@@ -2,6 +2,7 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 from odoo import models, api
+from odoo.tools.float_utils import float_round
 
 
 class SaleOrderLine(models.Model):
@@ -12,6 +13,16 @@ class SaleOrderLine(models.Model):
         self.ensure_one()
         res = super(SaleOrderLine, self)._prepare_invoice_line(qty)
         if self.secondary_uom_id:
-            res['secondary_uom_qty'] = self.secondary_uom_qty
+            res['secondary_uom_qty'] = self._get_secondary_uom_qty()
             res['secondary_uom_id'] = self.secondary_uom_id.id
         return res
+
+    @api.multi
+    def _get_secondary_uom_qty(self):
+        factor = self.secondary_uom_id.factor * \
+            self.product_uom.factor
+        rounding_factor = self.secondary_uom_id.uom_id.factor
+        return float_round(
+            self.qty_delivered / (factor or 1.0),
+            precision_rounding=rounding_factor
+        )
