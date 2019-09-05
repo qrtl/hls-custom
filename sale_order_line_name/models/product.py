bb42505b0f63cd6e2ebe7121c84c5f80f88ae787
Tomoko Matsumura <tomoko.m.0123@gmail.com>
2019-09-05 11:02:45 +0800

    [IMP] add comment

--- sale_order_line_name/models/product.py
+++ sale_order_line_name/models/product.py
@@ -5,6 +5,9 @@ from odoo import fields, models
 
 
 class ProductProduct(models.Model):
+    """ Override the method that compute a multiline description of this product, in the context of sales.
+        In order to display/output product name without reference code.
+    """
     _inherit = 'product.product'
 
     def get_product_multiline_description_sale(self):
