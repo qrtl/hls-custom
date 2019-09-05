06d13d5a3fbb46355172b5c1df3b6f72df782e44
Tomoko Matsumura <45782518+nakometal@users.noreply.github.com>
2019-09-05 14:56:31 +0900

    #90 Display product description without reference code in multiline. (#40)
    * [ADD] Override product.product model method. 
    * Display Product Name without reference code in Sale Order Line.

---
+++ sale_line_name_adj/__manifest__.py
@@ -0,0 +1,21 @@
+# Copyright 2019 Quartile Limited
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+{
+    "name": "Sale Line Name Adjust",
+    "summary": "",
+    "description": """
+ - Display Product description without reference code in Sale Order line.   
+    """,
+    'version': '12.0.1.0.0',
+    "category": "Sales",
+    "website": "https://www.quartile.co/",
+    "author": "Quartile Limited",
+    "license": "AGPL-3",
+    "application": False,
+    "installable": True,
+    "depends": [
+        "sale"
+    ],
+    "data": [
+    ],
+}
---
+++ sale_line_name_adj/models/product.py
@@ -0,0 +1,17 @@
+# Copyright 2019 Quartile Limited
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from odoo import models
+
+
+class ProductProduct(models.Model):
+    """ Override the method that compute a multiline description of this product, in the context of sales.
+        In order to display/output product description(sale.order.line.name)  without reference code.
+    """
+    _inherit = 'product.product'
+
+    def get_product_multiline_description_sale(self):
+        name = self.name
+        if self.description_sale:
+            name += '\n' + self.description_sale
+        return name
