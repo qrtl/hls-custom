878ecc92b61654ec713a92e7d514c617145bf74d
Tomoko Matsumura <45782518+nakometal@users.noreply.github.com>
2019-09-13 17:07:04 +0900

    [IMP] 403 Rename variable of secondary unit price (#44)
    * secondary_unit_price -> secondary_uom_price

--- sale_order_secondary_unit_price/models/sale_order_line.py
+++ sale_order_secondary_unit_price/models/sale_order_line.py
@@ -8,24 +8,24 @@ from odoo.addons import decimal_precision as dp
 class SaleOrderLine(models.Model):
     _inherit = 'sale.order.line'
 
-    secondary_unit_price = fields.Float(
+    secondary_uom_price = fields.Float(
         'Secondary Unit Price', 
         digits=dp.get_precision('Product Price')
     )
 
     @api.onchange('product_id')
     def product_id_change(self):
-        self.secondary_unit_price = 0
+        self.secondary_uom_price = 0
         super(SaleOrderLine, self).product_id_change()
 
-    @api.onchange('secondary_uom_id', 'secondary_unit_price')
+    @api.onchange('secondary_uom_id', 'secondary_uom_price')
     def onchange_secondary_price(self):
         if not self.secondary_uom_id:
-            self.secondary_unit_price = 0
+            self.secondary_uom_price = 0
             return
-        if self.secondary_unit_price:
+        if self.secondary_uom_price:
             factor = self.secondary_uom_id.factor * self.product_uom.factor
-            self.price_unit = self.secondary_unit_price / factor
+            self.price_unit = self.secondary_uom_price / factor
 
     @api.onchange('price_unit')
     def onchange_price_unit(self):
@@ -33,10 +33,10 @@ class SaleOrderLine(models.Model):
             already keeps a value. i.e. secondary unit price will show on
             printed invoice in case it is not 0.0.
         """
-        if not (self.secondary_uom_id and self.secondary_unit_price):
+        if not (self.secondary_uom_id and self.secondary_uom_price):
             return
         factor = self.secondary_uom_id.factor * self.product_uom.factor
-        self.secondary_unit_price = self.price_unit * factor
+        self.secondary_uom_price = self.price_unit * factor
 
     @api.onchange('product_uom', 'product_uom_qty')
     def product_uom_change(self):
