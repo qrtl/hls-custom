4904bf12e1f2bea30eaa591313794344d55be758
Tomoko Matsumura <tomoko.m.0123@gmail.com>
2019-09-17 11:59:50 +0800

    [WIP] add origin in view

---
+++ account_invoice_line/__manifest__.py
@@ -0,0 +1,22 @@
+# Copyright 2019 Quartile Limited
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+{
+    'name': 'Account Invoice Line ',
+    'version': '12.0.1.1.0',
+    'author': 'Quartile Limited',
+    'website': 'https://www.quartile.co',
+    'category': 'Accounting',
+    'license': "AGPL-3",
+    'description': """
+Adjust Account Invoice Line View
+- Display Sale Order Name
+- Default sort by Sale Order Name.
+    """,
+    'depends': [
+        'account',
+    ],
+    'data': [
+        'views/account_invoice_views.xml',
+    ],
+    'installable': True,
+}
---
+++ account_invoice_line/models/account_invoice_line.py
@@ -0,0 +1,9 @@
+# Copyright 2018 Tecnativa - Carlos Dauden
+# Copyright 2019 Quartile Limited
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from odoo import models, fields
+
+class AccountInvoiceLine(models.Model):
+    _inherit = 'account.invoice.line'
+    _order = 'origin ASC'
