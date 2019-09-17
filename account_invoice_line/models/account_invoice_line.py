2fa65095de8b084697ac57d46e7756fc5a1a66f6
Tomoko Matsumura <tomoko.m.0123@gmail.com>
2019-09-17 15:04:56 +0800

    [IMP] new line

--- account_invoice_line/models/account_invoice_line.py
+++ account_invoice_line/models/account_invoice_line.py
@@ -6,4 +6,4 @@ from odoo import models, fields
 
 class AccountInvoiceLine(models.Model):
     _inherit = 'account.invoice.line'
-    _order = "invoice_id,sequence desc,id desc"
+    _order = "invoice_id, sequence desc, id desc"
