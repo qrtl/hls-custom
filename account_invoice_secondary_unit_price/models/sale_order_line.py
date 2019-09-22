f0b905bfe9e2d3d1bed68fb572f9ede7adbb0f8c
Yoshi Tashiro <tashiro@quartile.co>
2019-09-22 15:46:25 +0900

    add account_invoice_secondary_unit_price (#49)

---
+++ account_invoice_secondary_unit_price/models/sale_order_line.py
@@ -0,0 +1,16 @@
+# Copyright 2019 Quartile Limited
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from odoo import models, api
+
+
+class SaleOrderLine(models.Model):
+    _inherit = 'sale.order.line'
+
+    @api.multi
+    def _prepare_invoice_line(self, qty):
+        self.ensure_one()
+        res = super(SaleOrderLine, self)._prepare_invoice_line(qty)
+        if self.secondary_uom_price:
+            res['secondary_uom_price'] = self.secondary_uom_price
+        return res
