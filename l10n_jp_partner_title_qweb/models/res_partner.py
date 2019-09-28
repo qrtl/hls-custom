23180baecbbfa96877d08cc69a3ad0ca723a5540
Yoshi Tashiro <tashiro@quartile.co>
2019-09-28 14:41:32 +0800

    disable _compute_title method

--- l10n_jp_partner_title_qweb/models/res_partner.py
+++ l10n_jp_partner_title_qweb/models/res_partner.py
@@ -15,24 +15,26 @@ class ResPartner(models.Model):
         readonly=False,
     )
 
-    @api.multi
-    @api.depends('lang', 'is_company')
-    def _compute_title(self):
-        for partner in self:
-            # handle the case NewId is returned for partner.id (i.e. onchange)
-            partner_id = self._origin.id if isinstance(
-                partner.id, models.NewId) else partner.id
-            # no title proposal for partners linked to internal users and
-            # companies (res.company)
-            if not partner.lang or partner.user_ids and \
-                    not partner.user_ids.share or \
-                    self.env['res.company'].search(
-                        [('partner_id', '=', partner_id)]):
-                partner.title = False
-            else:
-                lang_id = self.env['res.lang'].search(
-                    [('code', '=', partner.lang)], limit=1).id
-                title = self.env['res.partner.title'].search(
-                    [('lang_id', '=', lang_id),
-                     ('for_company', '=', partner.is_company)], limit=1)
-                partner.title = title.id if title else False
+    # following method is disabled since there are cases where "individual"
+    # in Odoo does not necessarily represent an individual.
+    # @api.multi
+    # @api.depends('lang', 'is_company')
+    # def _compute_title(self):
+    #     for partner in self:
+    #         # handle the case NewId is returned for partner.id (i.e. onchange)
+    #         partner_id = self._origin.id if isinstance(
+    #             partner.id, models.NewId) else partner.id
+    #         # no title proposal for partners linked to internal users and
+    #         # companies (res.company)
+    #         if not partner.lang or partner.user_ids and \
+    #                 not partner.user_ids.share or \
+    #                 self.env['res.company'].search(
+    #                     [('partner_id', '=', partner_id)]):
+    #             partner.title = False
+    #         else:
+    #             lang_id = self.env['res.lang'].search(
+    #                 [('code', '=', partner.lang)], limit=1).id
+    #             title = self.env['res.partner.title'].search(
+    #                 [('lang_id', '=', lang_id),
+    #                  ('for_company', '=', partner.is_company)], limit=1)
+    #             partner.title = title.id if title else False
