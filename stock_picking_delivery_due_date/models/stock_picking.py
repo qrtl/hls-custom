b49fd8fc8452970bc69b49a087ad393cd3d54fee
Yoshi Tashiro <tashiro@quartile.co>
2019-09-28 17:02:27 +0800

    add date_delivered field

--- stock_picking_delivery_due_date/models/stock_picking.py
+++ stock_picking_delivery_due_date/models/stock_picking.py
@@ -12,6 +12,22 @@ class StockPicking(models.Model):
         states={'done': [('readonly', True)], 'cancel': [('readonly', True)]},
         help="The date the customer is expected to receive the delivery."
     )
+    date_delivered = fields.Date(
+        'Delivered Date',
+        compute='_compute_date_delivered',
+        store=True,
+        help="Used to indicate the date on which the delivery was actually "
+             "received by customer. Manually adjust the date for completed "
+             "deliveries as necessary. This date is used to find target "
+             "deliveries in invoice print.",
+    )
+
+    @api.depends('delivery_due_date')
+    def _compute_date_delivered(self):
+        for pick in self.filtered(lambda x: x.delivery_due_date):
+            pick.date_delivered = fields.Date.to_date(
+                fields.Datetime.context_timestamp(
+                    pick, pick.delivery_due_date))
 
     @api.model
     def fields_view_get(self, view_id=None, view_type='form', toolbar=False,
