5f0bc725ac5d84a1ef8bbc970a1ea0e839181f3a
Yoshi Tashiro <tashiro@quartile.co>
2019-09-28 18:17:27 +0900

    90 upd stock picking delivery due date (#56)
    * add date_delivered field
    
    * add date_delivered to stock_move
    
    * adjust account_invoice_report_hls

--- account_invoice_report_hls/models/__init__.py
+++ account_invoice_report_hls/models/__init__.py
@@ -1,5 +1,3 @@
 from . import res_company
 from . import account_invoice_line
 from . import account_invoice
-from . import stock_move
-from . import stock_picking
--- stock_picking_delivery_due_date/models/stock_move.py
+++ stock_picking_delivery_due_date/models/stock_move.py
@@ -1,11 +1,17 @@
 # Copyright 2019 Quartile Limited
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from odoo import models
+from odoo import models, fields
 
 
 class StockMove(models.Model):
     _inherit = 'stock.move'
+
+    date_delivered = fields.Date(
+        'Delivered Date',
+        related='picking_id.date_delivered',
+        store=True,
+    )
 
     def _get_new_picking_values(self):
         vals = super(StockMove, self)._get_new_picking_values()
