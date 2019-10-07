3261723ed793969a4559eec5065454c2b7089557
Yoshi Tashiro <tashiro@quartile.co>
2019-10-07 17:58:03 +0800

    always propose secondary_uom_price

--- account_invoice_secondary_unit_price/models/account_invoice_line.py
+++ account_invoice_secondary_unit_price/models/account_invoice_line.py
@@ -13,11 +13,6 @@ class AccountInvoiceLine(models.Model):
         digits=dp.get_precision('Product Price')
     )
 
-    @api.onchange('product_id')
-    def _onchange_product_id(self):
-        self.secondary_uom_price = 0
-        super(AccountInvoiceLine, self)._onchange_product_id()
-
     @api.onchange('secondary_uom_id', 'secondary_uom_price')
     def onchange_secondary_price(self):
         if not self.secondary_uom_id:
@@ -29,11 +24,7 @@ class AccountInvoiceLine(models.Model):
 
     @api.onchange('price_unit')
     def onchange_price_unit(self):
-        """ secondary unit price should not be updated unless the field
-            already keeps a value. i.e. secondary unit price will show on
-            printed invoice in case it is not 0.0.
-        """
-        if not (self.secondary_uom_id and self.secondary_uom_price):
+        if not self.secondary_uom_id:
             return
         factor = self.secondary_uom_id.factor * self.uom_id.factor
         self.secondary_uom_price = self.price_unit * factor
