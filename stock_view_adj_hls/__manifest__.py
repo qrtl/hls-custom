0a6882d962264bd065f10f3fbe3d02c50f6e92a6
Sokream Phan <sokream@quartile.co>
2019-10-10 11:05:10 +0800

    [add new module] stock_view_adj_hls

---
+++ stock_view_adj_hls/__manifest__.py
@@ -0,0 +1,20 @@
+# Copyright 2019 Quartile Limited
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+{
+    "name": "Stock View Adj HLS",
+    "summary": "",
+    "description": """
+    """,
+    'version': '12.0.1.0.0',
+    "category": "Stock",
+    "website": "https://www.quartile.co/",
+    "author": "Quartile Limited",
+    "license": "AGPL-3",
+    "installable": True,
+    "depends": [
+         'stock_report_secondary_unit',
+    ],
+    "data": [
+        'views/stock_quant_views.xml',
+    ],
+}
