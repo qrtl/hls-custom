2af3ad980c022d754b72b288a97885cb91353515
Sokream Phan <sokream@quartile.co>
2019-10-10 15:09:44 +0800

    update manifest files of stock_report_secondary_unit and stock_view_adj_hls

--- stock_report_secondary_unit/__manifest__.py
+++ stock_report_secondary_unit/__manifest__.py
@@ -13,6 +13,7 @@ Adds secondary UoM and the converted qty in stock quant.
     'depends': [
         'sale_stock',
         'stock_secondary_unit',
+        'product_expiry',
     ],
     'data': [
         'views/stock_quant_views.xml',
--- stock_view_adj_hls/__manifest__.py
+++ stock_view_adj_hls/__manifest__.py
@@ -12,7 +12,7 @@
     "license": "AGPL-3",
     "installable": True,
     "depends": [
-         'stock_report_secondary_unit',
+         'product_expiry',
     ],
     "data": [
         'views/stock_quant_views.xml',
