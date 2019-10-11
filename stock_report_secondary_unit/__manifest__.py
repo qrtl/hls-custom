c304c614285f0664ffc20ea33f54e1ae624e32be
SokreamPhanqrl <56345535+SokreamPhanqrl@users.noreply.github.com>
2019-10-11 09:58:34 +0700

    517 upd stock report secondary unit (#61)
    * [ADD] removal_date field
    
    * new string translation
    
    * [Add] removal_date and adjust string
    
    * [add new module] stock_view_adj_hls
    
    * bump version of stock_report_secondary_unit
    
    * update manifest files of stock_report_secondary_unit and stock_view_adj_hls

--- stock_report_secondary_unit/__manifest__.py
+++ stock_report_secondary_unit/__manifest__.py
@@ -2,7 +2,7 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 {
     'name': 'Stock Report Scondary UoM ',
-    'version': '12.0.1.0.0',
+    'version': '12.0.1.0.1',
     'author': 'Quartile Limited',
     'website': 'https://www.quartile.co',
     'category': 'Stock',
@@ -13,6 +13,7 @@ Adds secondary UoM and the converted qty in stock quant.
     'depends': [
         'sale_stock',
         'stock_secondary_unit',
+        'product_expiry',
     ],
     'data': [
         'views/stock_quant_views.xml',
