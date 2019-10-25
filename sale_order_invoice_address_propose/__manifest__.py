ea025a074a93a078b25a41c34dce0bf38d22d203
SokreamPhanqrl <56345535+SokreamPhanqrl@users.noreply.github.com>
2019-10-25 17:02:45 +0700

    inherit sale_view_adj_hls (#68)
    * inherit sale_view_adj_hls
    
    * remove dulpicate line
    
    * add sale_view_adj_hls to dependency

--- sale_order_invoice_address_propose/__manifest__.py
+++ sale_order_invoice_address_propose/__manifest__.py
@@ -2,7 +2,7 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 {
     'name': 'Sale Order Invoice Address Propose',
-    'version': '12.0.1.0.1',
+    'version': '12.0.1.0.2',
     'author': 'Quartile Limited',
     'website': 'https://www.quartile.co',
     'category': 'Sales',
@@ -13,6 +13,7 @@ the sales order based on the selected Delivery Address.
     """,
     'depends': [
         'sale_stock',
+        'sale_view_adj_hls',
     ],
     'data': [
         'views/res_partner_views.xml',
