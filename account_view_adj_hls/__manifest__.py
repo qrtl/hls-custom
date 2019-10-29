c1dfcbb2b30ebc5f6034a8aab3a8d0324e4618d5
Yoshi Tashiro <tashiro@quartile.co>
2019-10-29 18:43:53 +0900

    add account_view_adj_hls (#71)

---
+++ account_view_adj_hls/__manifest__.py
@@ -0,0 +1,20 @@
+# Copyright 2019 Quartile Limited
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+{
+    "name": "Account View Adj HLS",
+    "summary": "",
+    "description": """
+    """,
+    'version': '12.0.1.0.0',
+    "category": "Accounting",
+    "website": "https://www.quartile.co/",
+    "author": "Quartile Limited",
+    "license": "AGPL-3",
+    "installable": True,
+    "depends": [
+        "account_invoice_report_hls",
+    ],
+    "data": [
+        'views/account_invoice_views.xml',
+    ],
+}
