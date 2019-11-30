3747997db446b6705e286a1c1361097eac937ad9
Yoshi Tashiro <tashiro@quartile.co>
2019-11-30 21:24:32 +0800

    black, isort

--- sale_order_check_status/__manifest__.py
+++ sale_order_check_status/__manifest__.py
@@ -9,10 +9,6 @@
     "author": "Quartile Limited",
     "license": "AGPL-3",
     "installable": True,
-    "depends": [
-        "sale_order_line_view",
-    ],
-    "data": [
-        "views/sale_order_views.xml",
-    ],
+    "depends": ["sale_order_line_view"],
+    "data": ["views/sale_order_views.xml"],
 }
