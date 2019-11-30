eae5f49739fcd7c438a4c23ab3c4bd64f4f11636
Yoshi Tashiro <tashiro@quartile.co>
2019-11-30 22:26:34 +0900

    Add sale order check status (#85)
    * add sale_order_check_status
    
    * black, isort

---
+++ sale_order_check_status/__manifest__.py
@@ -0,0 +1,14 @@
+# Copyright 2019 Quartile Limited
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+{
+    "name": "Sale Order Check Status",
+    "summary": "",
+    "version": "12.0.1.0.0",
+    "category": "Sales",
+    "website": "https://www.quartile.co/",
+    "author": "Quartile Limited",
+    "license": "AGPL-3",
+    "installable": True,
+    "depends": ["sale_order_line_view"],
+    "data": ["views/sale_order_views.xml"],
+}
