42887926084af079b3ce5232a4190b5c9f80fe7a
Yoshi Tashiro <tashiro@quartile.co>
2019-12-09 16:59:10 +0800

    upd show date_delivered field in receipt/transfer form

--- stock_picking_delivery_due_date/__manifest__.py
+++ stock_picking_delivery_due_date/__manifest__.py
@@ -2,7 +2,7 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 {
     'name': 'Stock Picking Delivery Due Date',
-    'version': '12.0.1.0.0',
+    'version': '12.0.1.0.1',
     'author': 'Quartile Limited',
     'website': 'https://www.quartile.co',
     'category': 'Stock',
