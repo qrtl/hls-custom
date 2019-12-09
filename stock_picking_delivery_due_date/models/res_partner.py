4bf443e0c84ee24e18689b6215a480a9992f79ef
Yoshi Tashiro <tashiro@quartile.co>
2019-12-09 17:03:18 +0800

    black

--- stock_picking_delivery_due_date/models/res_partner.py
+++ stock_picking_delivery_due_date/models/res_partner.py
@@ -1,15 +1,15 @@
 # Copyright 2019 Quartile Limited
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from odoo import models, fields
+from odoo import fields, models
 
 
 class ResPartner(models.Model):
-    _inherit = 'res.partner'
+    _inherit = "res.partner"
 
     delivery_lead = fields.Float(
-        'Delivery Lead Time',
+        "Delivery Lead Time",
         default=0.0,
         help="Delivery lead time in number of days. "
-             "Used in proposal of Delivery Due Date in deliveries.",
-        )
+        "Used in proposal of Delivery Due Date in deliveries.",
+    )
