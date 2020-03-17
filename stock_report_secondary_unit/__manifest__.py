659d24728a8e5e3971ca64016565149f96383731
Yoshi Tashiro <tashiro@quartile.co>
2020-03-17 22:38:54 +0800

    [IMP] black, isort

--- stock_report_secondary_unit/__manifest__.py
+++ stock_report_secondary_unit/__manifest__.py
@@ -1,22 +1,16 @@
 # Copyright 2019 Quartile Limited
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 {
-    'name': 'Stock Report Scondary UoM ',
-    'version': '12.0.1.0.1',
-    'author': 'Quartile Limited',
-    'website': 'https://www.quartile.co',
-    'category': 'Stock',
-    'license': "AGPL-3",
-    'description': """
+    "name": "Stock Report Scondary UoM ",
+    "version": "12.0.1.0.1",
+    "author": "Quartile Limited",
+    "website": "https://www.quartile.co",
+    "category": "Stock",
+    "license": "AGPL-3",
+    "description": """
 Adds secondary UoM and the converted qty in stock quant.
     """,
-    'depends': [
-        'sale_stock',
-        'stock_secondary_unit',
-        'product_expiry',
-    ],
-    'data': [
-        'views/stock_quant_views.xml',
-    ],
-    'installable': True,
+    "depends": ["sale_stock", "stock_secondary_unit", "product_expiry",],
+    "data": ["views/stock_quant_views.xml",],
+    "installable": True,
 }
--- stock_report_secondary_unit/models/stock_quant.py
+++ stock_report_secondary_unit/models/stock_quant.py
@@ -1,35 +1,31 @@
 # Copyright 2019 Quartile Limited
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from odoo import models, fields, api
+from odoo import api, fields, models
 from odoo.addons import decimal_precision as dp
 from odoo.tools.float_utils import float_round
 
 
 class StockQuant(models.Model):
-    _inherit = 'stock.quant'
+    _inherit = "stock.quant"
 
     secondary_uom_qty = fields.Float(
-        string='Secondary Qty',
-        digits=dp.get_precision('Product Unit of Measure'),
-        compute='_compute_product_secondary_uom_qty',
+        string="Secondary Qty",
+        digits=dp.get_precision("Product Unit of Measure"),
+        compute="_compute_product_secondary_uom_qty",
         store=True,
     )
     secondary_uom_id = fields.Many2one(
-        related='product_id.stock_secondary_uom_id',
-        string='Secondary UoM',
+        related="product_id.stock_secondary_uom_id", string="Secondary UoM",
     )
 
     @api.multi
-    @api.depends('quantity', 'product_id.stock_secondary_uom_id')
+    @api.depends("quantity", "product_id.stock_secondary_uom_id")
     def _compute_product_secondary_uom_qty(self):
-        for quant in self.filtered(
-                lambda x: x.product_id.stock_secondary_uom_id):
+        for quant in self.filtered(lambda x: x.product_id.stock_secondary_uom_id):
             uom = quant.product_uom_id
-            factor = quant.product_id.stock_secondary_uom_id.factor \
-                * uom.factor
+            factor = quant.product_id.stock_secondary_uom_id.factor * uom.factor
             quant.secondary_uom_qty = float_round(
                 quant.quantity / (factor or 1.0),
-                precision_rounding=quant.product_id.\
-                    stock_secondary_uom_id.uom_id.rounding
+                precision_rounding=quant.product_id.stock_secondary_uom_id.uom_id.rounding,
             )
