d80d7bdc392cdaf51e5957c3a1dc9e769ba256e2
Yoshi Tashiro <tashiro@quartile.co>
2020-03-18 15:49:41 +0800

    [IMP] purchase_view_adj_hls: black

--- purchase_view_adj_hls/__manifest__.py
+++ purchase_view_adj_hls/__manifest__.py
@@ -11,10 +11,6 @@
     "author": "Quartile Limited",
     "license": "AGPL-3",
     "installable": True,
-    "depends": [
-        "purchase_stock"
-    ],
-    "data": [
-        'views/purchase_order_views.xml',
-    ],
+    "depends": ["purchase_stock"],
+    "data": ["views/purchase_order_views.xml",],
 }
