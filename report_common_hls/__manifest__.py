d16bf41f73e2276d7600c1149eaf9f52bdf39b19
Yoshi Tashiro <tashiro@quartile.co>
2020-03-18 15:48:25 +0800

    [IMP] report_common_hls: black

--- report_common_hls/__manifest__.py
+++ report_common_hls/__manifest__.py
@@ -1,20 +1,15 @@
 # Copyright 2019 Quartile Limited
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 {
-    'name': 'Report Common',
-    'version': '12.0.1.0.0',
-    'category': 'Reporting',
-    'license': 'AGPL-3',
-    'description': """
+    "name": "Report Common",
+    "version": "12.0.1.0.0",
+    "category": "Reporting",
+    "license": "AGPL-3",
+    "description": """
 Adds common fields used in reports.
     """,
-    'author': 'Quartile Limited',
-    'depends': [
-        'base',
-    ],
-    'data': [
-        'views/res_company_views.xml',
-        'views/res_partner_views.xml',
-    ],
-    'installable': True,
+    "author": "Quartile Limited",
+    "depends": ["base",],
+    "data": ["views/res_company_views.xml", "views/res_partner_views.xml",],
+    "installable": True,
 }
