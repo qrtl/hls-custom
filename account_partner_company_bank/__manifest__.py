cfb791d25c43ae080a3863ae3a5245e1248c0d5c
Yoshi Tashiro <tashiro@quartile.co>
2020-03-18 15:53:28 +0800

    [IMP] account_partner_company_bank: black

--- account_partner_company_bank/__manifest__.py
+++ account_partner_company_bank/__manifest__.py
@@ -1,19 +1,15 @@
 # Copyright 2019 Quartile Limited
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 {
-    'name': 'Partner Company Bank',
-    'version': '12.0.1.0.0',
-    'category': 'Accounting',
-    'license': 'AGPL-3',
-    'description': """
+    "name": "Partner Company Bank",
+    "version": "12.0.1.0.0",
+    "category": "Accounting",
+    "license": "AGPL-3",
+    "description": """
 Adds Remit-to Bank Account field in partner.
     """,
-    'author': 'Quartile Limited',
-    'depends': [
-        'account',
-    ],
-    'data': [
-        'views/res_partner_views.xml',
-    ],
-    'installable': True,
+    "author": "Quartile Limited",
+    "depends": ["account",],
+    "data": ["views/res_partner_views.xml",],
+    "installable": True,
 }
