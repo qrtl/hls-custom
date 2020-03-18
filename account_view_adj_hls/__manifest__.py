dfd3074569dad8dd92335cb8e133b5ecbbec4309
Yoshi Tashiro <tashiro@quartile.co>
2020-03-18 17:09:19 +0800

    [IMP] account_view_adj_hls: pylint

--- account_view_adj_hls/__manifest__.py
+++ account_view_adj_hls/__manifest__.py
@@ -3,8 +3,6 @@
 {
     "name": "Account View Adj HLS",
     "summary": "",
-    "description": """
-    """,
     "version": "12.0.1.0.0",
     "category": "Accounting",
     "website": "https://www.quartile.co/",
