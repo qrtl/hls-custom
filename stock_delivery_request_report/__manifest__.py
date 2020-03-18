39371bbd78f3adfacf1257397c5de959c3116545
Yoshi Tashiro <tashiro@quartile.co>
2020-03-18 15:14:29 +0800

    [IMP] stock_delivery_request_report: black, isort

--- stock_delivery_request_report/__manifest__.py
+++ stock_delivery_request_report/__manifest__.py
@@ -1,29 +1,29 @@
 # Copyright 2019 Quartile Limited
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 {
-    'name': 'Stock Delivery Request Report',
-    'version': '12.0.1.1.1',
-    'author': 'Quartile Limited',
-    'website': 'https://www.quartile.co',
-    'category': 'Stock',
-    'license': "AGPL-3",
-    'description': """
+    "name": "Stock Delivery Request Report",
+    "version": "12.0.1.1.1",
+    "author": "Quartile Limited",
+    "website": "https://www.quartile.co",
+    "category": "Stock",
+    "license": "AGPL-3",
+    "description": """
 - This module adds a py3o delivery request report based on stock picking batch.
 - Adjust stock picking batch view.
 - Print Request Delivery form in Batch Picking.
     """,
-    'depends': [
-        'stock_picking_batch',
-        'delivery',
-        'report_py3o',
-        'stock_picking_delivery_due_date',
+    "depends": [
+        "stock_picking_batch",
+        "delivery",
+        "report_py3o",
+        "stock_picking_delivery_due_date",
     ],
-    'data': [
-        'report/stock_picking_report.xml',
-        'views/delivery_views.xml',
-        'views/stock_picking_views.xml',
-        'views/stock_picking_batch_views.xml',
-        'wizard/stock_picking_report_wizard_views.xml',
+    "data": [
+        "report/stock_picking_report.xml",
+        "views/delivery_views.xml",
+        "views/stock_picking_views.xml",
+        "views/stock_picking_batch_views.xml",
+        "wizard/stock_picking_report_wizard_views.xml",
     ],
-    'installable': True,
+    "installable": True,
 }
--- stock_delivery_request_report/models/stock_picking_batch.py
+++ stock_delivery_request_report/models/stock_picking_batch.py
@@ -1,25 +1,21 @@
 # Copyright 2019 Quartile Limited
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from odoo import models, fields, api
+from odoo import api, fields, models
+
 
 class StockPickingBatch(models.Model):
-    _inherit = 'stock.picking.batch'
+    _inherit = "stock.picking.batch"
 
-    carrier_id = fields.Many2one(
-        'delivery.carrier'
-    )
+    carrier_id = fields.Many2one("delivery.carrier")
     ship_date = fields.Date(
-        'Shipping Date',
-        default=fields.Date.context_today,
-        copy=False,
+        "Shipping Date", default=fields.Date.context_today, copy=False,
     )
-    user_id = fields.Many2one(
-        default=lambda self: self.env.user,
-    )
+    user_id = fields.Many2one(default=lambda self: self.env.user,)
     instruction = fields.Text()
 
-    @api.onchange('carrier_id')
+    @api.onchange("carrier_id")
     def _onchange_carrier_id(self):
-        self.instruction = self.carrier_id.default_instruction if \
-            self.carrier_id else False
+        self.instruction = (
+            self.carrier_id.default_instruction if self.carrier_id else False
+        )
