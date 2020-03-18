68c9f8c0dd28352c75d5938cf066444120706b19
Yoshi Tashiro <tashiro@quartile.co>
2020-03-18 15:10:12 +0800

    [IMP] stock_move_sale_invoice_partner: black, isort

--- stock_move_sale_invoice_partner/__manifest__.py
+++ stock_move_sale_invoice_partner/__manifest__.py
@@ -1,21 +1,16 @@
 # Copyright 2019 Quartile Limited
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 {
-    'name': 'Display Customer and Invoice Address on Stock Move',
-    'version': '12.0.1.0.0',
-    'author': 'Quartile Limited',
-    'website': 'https://www.quartile.co',
-    'category': 'Stock',
-    'license': "AGPL-3",
-    'description': """
+    "name": "Display Customer and Invoice Address on Stock Move",
+    "version": "12.0.1.0.0",
+    "author": "Quartile Limited",
+    "website": "https://www.quartile.co",
+    "category": "Stock",
+    "license": "AGPL-3",
+    "description": """
 Display the customer of sales order and invoice in stock move list view.
     """,
-    'depends': [
-        'stock',
-        'sale_stock',
-    ],
-    'data': [
-        'views/stock_move_views.xml',
-    ],
-    'installable': True,
+    "depends": ["stock", "sale_stock",],
+    "data": ["views/stock_move_views.xml",],
+    "installable": True,
 }
--- stock_move_sale_invoice_partner/models/stock_move.py
+++ stock_move_sale_invoice_partner/models/stock_move.py
@@ -1,33 +1,34 @@
 # Copyright 2019 Quartile Limited
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from odoo import models, fields, api
+from odoo import api, fields, models
 
 
 class StockMove(models.Model):
-    _inherit = 'stock.move'
+    _inherit = "stock.move"
 
     sale_order_partner_id = fields.Many2one(
-        related='sale_line_id.order_id.partner_id',
-        string='Sales Order Customer',
+        related="sale_line_id.order_id.partner_id", string="Sales Order Customer",
     )
     invoice_partner_id = fields.Many2one(
-        'res.partner',
-        compute='_get_invoice_partner_id',
+        "res.partner",
+        compute="_get_invoice_partner_id",
         store=True,
-        string='Invoice Customer',
+        string="Invoice Customer",
     )
 
     @api.multi
-    @api.depends('sale_line_id',
-                 'sale_line_id.order_id.partner_invoice_id',
-                 'sale_line_id.order_id.invoice_ids.partner_id')
+    @api.depends(
+        "sale_line_id",
+        "sale_line_id.order_id.partner_invoice_id",
+        "sale_line_id.order_id.invoice_ids.partner_id",
+    )
     def _get_invoice_partner_id(self):
         for move in self:
             if move.sale_line_id:
-                move.invoice_partner_id = \
-                    move.sale_line_id.order_id.partner_invoice_id
+                move.invoice_partner_id = move.sale_line_id.order_id.partner_invoice_id
                 invoices = move.sale_line_id.order_id.invoice_ids.filtered(
-                    lambda s: s.state != 'cancel')
+                    lambda s: s.state != "cancel"
+                )
                 if invoices:
                     move.invoice_partner_id = invoices[0].partner_id
