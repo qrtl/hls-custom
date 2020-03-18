8517ee59b22c6e7e53a8ccff3428dddb4eb75346
Yoshi Tashiro <tashiro@quartile.co>
2020-03-18 15:57:15 +0800

    [IMP] account_invoice_secondary_unit: black, isort

--- account_invoice_secondary_unit/__manifest__.py
+++ account_invoice_secondary_unit/__manifest__.py
@@ -2,22 +2,17 @@
 # Copyright 2019 Quartile Limited
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 {
-    'name': 'Account Invoice Scondary UoM ',
-    'version': '12.0.1.1.0',
-    'author': 'Quartile Limited',
-    'website': 'https://www.quartile.co',
-    'category': 'Accounting',
-    'license': "AGPL-3",
-    'description': """
+    "name": "Account Invoice Scondary UoM ",
+    "version": "12.0.1.1.0",
+    "author": "Quartile Limited",
+    "website": "https://www.quartile.co",
+    "category": "Accounting",
+    "license": "AGPL-3",
+    "description": """
 Adds secondary UoM and the converted qty in account invoice.
 This module borrows much of the logic from OCA sale_order_secondary_unit.
     """,
-    'depends': [
-        'account',
-        'sale_order_secondary_unit',
-    ],
-    'data': [
-        'views/account_invoice_views.xml',
-    ],
-    'installable': True,
+    "depends": ["account", "sale_order_secondary_unit",],
+    "data": ["views/account_invoice_views.xml",],
+    "installable": True,
 }
