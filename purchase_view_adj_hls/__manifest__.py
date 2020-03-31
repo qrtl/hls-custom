0acc5b5be1a932660e86bd39ab775d5c6bab6a42
Yoshi Tashiro <tashiro@quartile.co>
2020-03-31 22:09:53 +0900

    [FIX] purchase_view_adj_hls: default value proposal for picking_type_id (#94)

--- purchase_view_adj_hls/__manifest__.py
+++ purchase_view_adj_hls/__manifest__.py
@@ -1,13 +1,13 @@
-# Copyright 2019 Quartile Limited
-# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+# Copyright 2019-2020 Quartile Limited
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
 {
     "name": "Purchase View Adj HLS",
     "summary": "",
-    "version": "12.0.1.0.1",
+    "version": "12.0.1.0.2",
     "category": "Purchase",
     "website": "https://www.quartile.co/",
     "author": "Quartile Limited",
-    "license": "AGPL-3",
+    "license": "LGPL-3",
     "installable": True,
     "depends": ["purchase_stock"],
     "data": ["views/purchase_order_views.xml"],
