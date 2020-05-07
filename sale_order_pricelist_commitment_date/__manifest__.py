4bc675cf8db7f3f174ee3b4c6bb620040c0a35e9
YogeshMahera-SerpentCS <yogesh.mahera.serpentcs@gmail.com>
2020-05-07 20:27:26 +0530

    [ADD] Added sale_order_pricelist_commitment_date module

---
+++ sale_order_pricelist_commitment_date/__manifest__.py
@@ -0,0 +1,15 @@
+# Copyright 2020 Quartile Limited
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+{
+    "name": "Sale Order Pricelist Commitment Date",
+    "summary": "",
+    "version": "12.0.1.0.0",
+    "category": "Sales",
+    "website": "https://www.quartile.co/",
+    "author": "Quartile Limited",
+    "license": "LGPL-3",
+    "application": False,
+    "installable": True,
+    "depends": ["sale"],
+    "data": ["views/sale_view.xml"],
+}
