9e0d2bb0a1b3debdfe47714e0d78300adafc4d55
YogeshMahera-SerpentCS <yogesh.mahera.serpentcs@gmail.com>
2020-05-08 13:37:54 +0530

    [FIX]Pylint

--- sale_order_pricelist_commitment_date/models/product_pricelist.py
+++ sale_order_pricelist_commitment_date/models/product_pricelist.py
@@ -1,6 +1,5 @@
 # Copyright 2020 Quartile Limited
 # License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
-from itertools import chain
 
 from odoo import api, fields, models
 
@@ -10,8 +9,12 @@ class Pricelist(models.Model):
 
     @api.multi
     def _compute_price_rule(self, products_qty_partner, date=False, uom_id=False):
-        date = self._context.get(
-            "commitment_date") or self._context.get("date") or fields.Date.today()
+        date = (
+            self._context.get("commitment_date")
+            or self._context.get("date")
+            or fields.Date.today()
+        )
         date = fields.Date.to_date(date)
         return super(Pricelist, self)._compute_price_rule(
-            products_qty_partner=products_qty_partner, date=date, uom_id=uom_id)
+            products_qty_partner=products_qty_partner, date=date, uom_id=uom_id
+        )
