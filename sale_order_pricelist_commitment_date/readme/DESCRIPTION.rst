e9329aa32b4fdf3870877f47c1b179a1dcdc24a6
Yogesh Mahera <yogesh.mahera.serpentcs@gmail.com>
2020-05-12 19:39:35 +0530

    Merge pull request #97 from YogeshMahera-SerpentCS/T968
    [12.0][T968][ADD]- Added Sale Order Pricelist commitment date module

---
+++ sale_order_pricelist_commitment_date/readme/DESCRIPTION.rst
@@ -0,0 +1,3 @@
+This module does the following:
+
+- Allows the user a calculate the product pricelist based on sales-order commitment date.
---
+++ sale_order_pricelist_commitment_date/tests/test_sale_pricelist.py
@@ -0,0 +1,139 @@
+# Copyright 2020 Quartile Limited
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
+
+from datetime import date, timedelta
+
+from dateutil.relativedelta import relativedelta
+from odoo.tests import common, tagged
+from odoo.tools import DEFAULT_SERVER_DATE_FORMAT
+
+
+@tagged("post_install", "-at_install")
+class TestWebsiteSaleComment(common.TransactionCase):
+    def setUp(self):
+        super(TestWebsiteSaleComment, self).setUp()
+        partner = self.env["res.partner"].create(dict(name="George"))
+        SaleOrder = self.env["sale.order"]
+        self.SaleOrderLine = self.env["sale.order.line"]
+        self.product_id = self.env.ref("product.product_product_1")
+        self.product_id.write({"lst_price": 10})
+        today = date.today()
+        next_week = today + timedelta(weeks=1)
+
+        self.pricelist_usd = self.env["product.pricelist"].create(
+            {
+                "name": "USD pricelist",
+                "active": True,
+                "currency_id": self.env.ref("base.USD").id,
+                "company_id": self.env.user.company_id.id,
+                "item_ids": [
+                    (
+                        0,
+                        0,
+                        {
+                            "compute_price": "fixed",
+                            "fixed_price": 100,
+                            "date_start": today.strftime(DEFAULT_SERVER_DATE_FORMAT),
+                            "date_end": next_week.strftime(DEFAULT_SERVER_DATE_FORMAT),
+                        },
+                    )
+                ],
+            }
+        )
+        self.commitment_date = date.today() + relativedelta(days=+5)
+        self.sale_order = SaleOrder.create(
+            {"partner_id": partner.id, "date_order": date.today()}
+        )
+
+    def test_compute_price_rule(self):
+        """
+            This function will check the commitment date is passed correctly or not.
+        """
+        self.sale_order.write(
+            {
+                "commitment_date": self.commitment_date,
+                "pricelist_id": self.pricelist_usd.id,
+            }
+        )
+        self.so_line = self.SaleOrderLine.with_context(
+            commitment_date=self.commitment_date
+        ).create(
+            {
+                "product_id": self.product_id.id,
+                "product_uom_qty": 1,
+                "product_uom": self.product_id.uom_id.id,
+                "price_unit": self.product_id.list_price,
+                "order_id": self.sale_order.id,
+                "tax_id": False,
+            }
+        )
+        self.sale_order.onchange_commitment_date()
+
+        # it will check the commitment data with value of context commitement date
+        self.assertEquals(
+            self.so_line._context["commitment_date"],
+            self.commitment_date,
+            "In Compute Price rule the commitment date pass date through context",
+        )
+
+        # it will compare the price unit with updated
+        # price unit based on commitment date
+        self.assertEquals(
+            self.so_line.price_unit,
+            100,
+            "It compare the price unit with fixed price in price list.",
+        )
+
+    def test_onchange_commitment_date(self):
+        """
+            This function will check the price unit based
+             on price list and commitment date.
+        """
+        self.so_line = self.SaleOrderLine.create(
+            {
+                "product_id": self.product_id.id,
+                "product_uom_qty": 1,
+                "product_uom": self.product_id.uom_id.id,
+                "price_unit": self.product_id.list_price,
+                "order_id": self.sale_order.id,
+                "tax_id": False,
+            }
+        )
+
+        # Compare the Sale Order line
+        # price unit with original price unit
+        self.assertEquals(
+            self.so_line.price_unit,
+            self.product_id.list_price,
+            "It will check with original product price"
+            " unit and compare with product list price",
+        )
+        self.commitment_date = date.today() + relativedelta(days=+3)
+        self.sale_order.write(
+            {
+                "pricelist_id": self.pricelist_usd.id,
+                "commitment_date": self.commitment_date,
+            }
+        )
+        self.sale_order.onchange_commitment_date()
+
+        # Compare the Sale Order line price unit with updated price list
+        # which check does the commitment date covers this or not
+        self.assertEquals(
+            self.so_line.price_unit,
+            100,
+            "It compare the price unit with fixed price in price list.",
+        )
+        self.commitment_date = date.today() + relativedelta(days=+10)
+        self.sale_order.write({"commitment_date": self.commitment_date})
+        self.sale_order.onchange_commitment_date()
+
+        # Compare the Sale Order line price unit with product list price
+        # it not covers the commitment date
+        # it should be original product price unit on so line
+        self.assertEquals(
+            self.so_line.price_unit,
+            self.product_id.list_price,
+            "It will check with original product price"
+            " unit and compare with product list price",
+        )
