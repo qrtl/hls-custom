759c423b80ab422b0571211b6d2bdda951c4175e
Yoshi Tashiro <tashiro@quartile.co>
2020-06-15 23:35:00 +0800

    update readme

--- stock_move_line_removal_date/__manifest__.py
+++ stock_move_line_removal_date/__manifest__.py
@@ -2,7 +2,7 @@
 # License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
 {
     "name": "Stock Move Line Removal Date",
-    "version": "12.0.1.0.1",
+    "version": "12.0.1.0.0",
     "author": "Quartile Limited",
     "website": "https://www.quartile.co",
     "category": "Stock",
