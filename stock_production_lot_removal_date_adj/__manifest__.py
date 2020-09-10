0412fde50f9c55a8771a08f602d7083bb4ab433e
tslai <tl@quartile.co>
2020-09-10 16:29:02 +0800

    [IMP] Fix module name

--- stock_production_lot_removal_date_adj/__manifest__.py
+++ stock_production_lot_removal_date_adj/__manifest__.py
@@ -1,7 +1,7 @@
 # Copyright 2020 Quartile Limited
 # License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
 {
-    "name": "Stock Production Lot Name Adj",
+    "name": "Stock Production Lot Removal Date Adj",
     "version": "12.0.1.0.0",
     "author": "Quartile Limited",
     "website": "https://www.quartile.co",
