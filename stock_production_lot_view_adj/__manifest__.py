48f04b8993dac64ce8942efeb87c5ba138a1afca
Tim <tl@quartile.co>
2020-09-11 17:26:34 +0800

    [1018][ADD] stock_production_lot_view_adj (#107)

---
+++ stock_production_lot_view_adj/__manifest__.py
@@ -0,0 +1,13 @@
+# Copyright 2020 Quartile Limited
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
+{
+    "name": "Stock Production Lot Views Adjustments",
+    "version": "12.0.1.0.0",
+    "author": "Quartile Limited",
+    "website": "https://www.quartile.co",
+    "category": "Stock",
+    "license": "LGPL-3",
+    "depends": ["product_expiry"],
+    "data": ["views/stock_production_lot_views.xml"],
+    "installable": True,
+}
