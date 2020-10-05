f35d9ddfdd646d7eac62bcb2b571d1de46e2d230
tslai <tl@quartile.co>
2020-10-05 17:17:38 +0800

    [UPD] Rename module, update license

--- sale_order_value_proposal_by_partner_shipping/__manifest__.py
+++ sale_order_value_proposal_by_partner_shipping/__manifest__.py
@@ -1,12 +1,12 @@
 # Copyright 2020 Quartile Limited
-# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
 {
-    "name": "Sale Order Delivery Address Propose",
+    "name": "Sale Order Value Proposal By Delivery Address",
     "version": "12.0.1.0.0",
     "author": "Quartile Limited",
     "website": "https://www.quartile.co",
     "category": "Sales",
-    "license": "AGPL-3",
+    "license": "LGPL-3",
     "depends": ["delivery"],
     "data": ["views/res_partner_views.xml"],
     "installable": True,
