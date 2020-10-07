e669901d0732028ab1baa845c84252d3971d988e
Tim <tl@quartile.co>
2020-10-07 09:36:38 +0800

    [1216][ADD] sale_order_value_proposal_by_partner_shipping (#108)

---
+++ sale_order_value_proposal_by_partner_shipping/__manifest__.py
@@ -0,0 +1,13 @@
+# Copyright 2020 Quartile Limited
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
+{
+    "name": "Sale Order Value Proposal By Delivery Address",
+    "version": "12.0.1.0.0",
+    "author": "Quartile Limited",
+    "website": "https://www.quartile.co",
+    "category": "Sales",
+    "license": "LGPL-3",
+    "depends": ["delivery"],
+    "data": ["views/res_partner_views.xml"],
+    "installable": True,
+}
---
+++ sale_order_value_proposal_by_partner_shipping/models/sale_order.py
@@ -0,0 +1,26 @@
+# Copyright 2020 Quartile Limited
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
+
+from odoo import api, models
+
+
+class SaleOrder(models.Model):
+    _inherit = "sale.order"
+
+    @api.onchange("partner_shipping_id")
+    def _onchange_partner_shipping_id(self):
+        res = super(SaleOrder, self)._onchange_partner_shipping_id()
+        partner = self.partner_shipping_id
+        if partner:
+            if partner.user_id:
+                self.user_id = partner.user_id
+            if partner.warehouse_id:
+                self.warehouse_id = partner.warehouse_id
+            self.carrier_id = partner.property_delivery_carrier_id.filtered("active")
+        return res
+
+    @api.onchange("partner_id")
+    def onchange_partner_id_carrier_id(self):
+        # if self.partner_id:
+        #     self.carrier_id = self.partner_id.property_delivery_carrier_id.filtered('active') # noqa
+        return
