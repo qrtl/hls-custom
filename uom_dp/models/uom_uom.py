c3e3d24a8ffc8023cd00936d61ec7b96636870d6
tslai <tl@quartile.co>
2020-11-20 14:07:11 +0800

    [ADD] uom_dp

---
+++ uom_dp/models/uom_uom.py
@@ -0,0 +1,15 @@
+# Copyright 2020 Quartile Limited
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
+
+from odoo import api, fields, models
+
+
+class UoM(models.Model):
+    _inherit = 'uom.uom'
+
+    uom_dp = fields.Char('Demical Places', compute='_compute_uom_dp')
+    
+    @api.multi
+    def _compute_uom_dp(self):
+        for uom in self:
+            uom.uom_dp = str(uom.rounding)[::-1].find(".")
