feed4e75afe61ec8c364c3b214b6afefc6a37f2c
Tim <tl@quartile.co>
2020-11-23 13:17:02 +0800

    [1432][FIX] Rounding of secondary qty (#115)

---
+++ uom_dp/__manifest__.py
@@ -0,0 +1,12 @@
+# Copyright 2020 Quartile Limited
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
+{
+    "name": "Unit of Measure's Demical Places",
+    "version": "12.0.1.0.0",
+    "category": "base",
+    "license": "LGPL-3",
+    "author": "Quartile Limited",
+    "depends": ["uom"],
+    "data": [],
+    "installable": True,
+}
---
+++ uom_dp/readme/DESCRIPTION.rst
@@ -0,0 +1,3 @@
+This module does the following:
+
+- Adds uom_dp to UoM which indicates the demical places to be shown based on the rounding.
