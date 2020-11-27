316dba30a58e29e289a55d2e8f78dfb8642a794d
tslai <tl@quartile.co>
2020-11-27 22:06:35 +0800

    [ADD] sale_order_delivery_date

---
+++ sale_order_delivery_date/models/sale_order.py
@@ -0,0 +1,16 @@
+# Copyright 2020 Quartile Limited
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
+
+from odoo import fields, models
+
+
+class SaleOrder(models.Model):
+    _inherit = "sale.order"
+
+    delivery_expected_date = fields.Date(
+        "Expected Delivery Date"
+    )
+
+    def _get_due_date(self):
+        self.ensure_one()
+        return self.commitment_date or self.expected_date
---
+++ sale_order_delivery_date/models/sale_order_line.py
@@ -0,0 +1,19 @@
+# Copyright 2020 Quartile Limited
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
+
+from odoo import api, fields, models
+from pytz import timezone, UTC
+
+
+class SaleOrderLine(models.Model):
+    _inherit = "sale.order.line"
+
+    @api.multi
+    def _prepare_procurement_values(self, group_id=False):
+        values = super(SaleOrderLine, self)._prepare_procurement_values(group_id)
+        self.ensure_one()
+        tz = self.env.user.tz
+        expect_datetime = fields.Datetime.from_string(self.order_id.delivery_expected_date)
+        date_planned = fields.Datetime.to_string(timezone(tz).localize(expect_datetime).astimezone(UTC))
+        values.update({"date_planned": date_planned})
+        return values
