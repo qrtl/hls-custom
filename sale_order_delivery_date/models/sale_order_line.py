f3799577b50e4b1e58af00a5c330eefa7f487ddb
Tim <tl@quartile.co>
2020-12-02 16:46:17 +0800

    [1470][ADD] sale_order_delivery_date (#116)

---
+++ sale_order_delivery_date/models/sale_order_line.py
@@ -0,0 +1,25 @@
+# Copyright 2020 Quartile Limited
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
+
+from dateutil.relativedelta import relativedelta
+from odoo import api, fields, models
+from pytz import UTC, timezone
+
+
+class SaleOrderLine(models.Model):
+    _inherit = "sale.order.line"
+
+    @api.multi
+    def _prepare_procurement_values(self, group_id=False):
+        values = super(SaleOrderLine, self)._prepare_procurement_values(group_id)
+        tz = self.env.user.tz
+        if self.order_id.dispatch_expected_date:
+            expect_datetime = fields.Datetime.from_string(
+                self.order_id.dispatch_expected_date
+            )
+            expect_datetime = expect_datetime + relativedelta(hours=12)
+            date_planned = fields.Datetime.to_string(
+                timezone(tz).localize(expect_datetime).astimezone(UTC)
+            )
+            values.update({"date_planned": date_planned})
+        return values
