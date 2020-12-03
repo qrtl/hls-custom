357b2bc791724c9be5b51c4f33307b272e389518
Tim <tl@quartile.co>
2020-12-03 16:28:46 +0800

    [FIX] date_delivered key error (#118)

--- sale_order_delivery_date/models/stock_move.py
+++ sale_order_delivery_date/models/stock_move.py
@@ -1,11 +1,15 @@
 # Copyright 2020 Quartile Limited
 # License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
 
-from odoo import models
+from odoo import fields, models
 
 
 class StockMove(models.Model):
     _inherit = "stock.move"
+
+    date_delivered = fields.Date(
+        "Delivered Date", related="picking_id.date_delivered", store=True
+    )
 
     def _get_new_picking_values(self):
         vals = super(StockMove, self)._get_new_picking_values()
