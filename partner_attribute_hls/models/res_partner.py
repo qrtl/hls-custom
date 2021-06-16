b1deca86dfc881c5638894f93ce031179c78380c
kakurai8 <80655511+kakurai8@users.noreply.github.com>
2021-06-16 19:01:20 +0900

    add help
    Co-authored-by: Yoshi Tashiro (Quartile) <tashiro@quartile.co>

--- partner_attribute_hls/models/res_partner.py
+++ partner_attribute_hls/models/res_partner.py
@@ -6,4 +6,4 @@ from odoo import fields, models
 class ResPartner(models.Model):
     _inherit = "res.partner"
 
-    customer_code = fields.Char(copy=False)
+    customer_code = fields.Char(copy=False, help="Intended to be passed to the external accounting system.")
