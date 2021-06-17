2e9242c5b7e78e35dbd9553017f4ac2fbddc4624
kakurai8 <80655511+kakurai8@users.noreply.github.com>
2021-06-17 20:20:52 +0900

    st2 [1803][ADD] partner_attribute_hls (#136)
    * add_partner_attribute_hls

---
+++ partner_attribute_hls/__manifest__.py
@@ -0,0 +1,13 @@
+# Copyright 2021 Quartile Limited
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
+{
+    "name": "Partner Attribute HLS",
+    "version": "12.0.1.0.0",
+    "category": "Contact",
+    "website": "https://www.quartile.co/",
+    "author": "Quartile Limited",
+    "license": "LGPL-3",
+    "installable": True,
+    "depends": ["base"],
+    "data": ["views/res_partner_views.xml"],
+}
---
+++ partner_attribute_hls/readme/DESCRIPTION.rst
@@ -0,0 +1,3 @@
+This module does the following:
+
+- Adds fields to the partner model.
