1b0bf62fe8e31eb74d2c88d31cf67fa716e643b8
TomohiroAbe-Quartile <tomohiro@quartile.co>
2021-07-14 12:57:25 +0800

    Add out_going group

--- sale_order_delivery_date/models/stock_picking.py
+++ sale_order_delivery_date/models/stock_picking.py
@@ -23,9 +23,23 @@ class StockPicking(models.Model):
         "deliveries in invoice print.",
     )
 
+    is_customer_outgoing = fields.Boolean(
+        compute="_compute_is_customer_outgoing", store=True
+    )
+
     @api.depends("delivery_due_date")
     def _compute_date_delivered(self):
         for pick in self.filtered(lambda x: x.delivery_due_date):
             pick.date_delivered = fields.Date.to_date(
                 fields.Datetime.context_timestamp(pick, pick.delivery_due_date)
             )
+
+    @api.depends("picking_type_code", "location_dest_id")
+    def _compute_is_customer_outgoing(self):
+        for pick in self:
+            if (
+                pick.picking_type_code == "outgoing"
+                and pick.location_dest_id
+                and pick.location_dest_id.usage == "customer"
+            ):
+                pick.is_customer_outgoing = True
