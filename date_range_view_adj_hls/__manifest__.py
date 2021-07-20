1881d4af3ff76fb058060866ab13857348c68113
TomohiroAbe-Quartile <tomohiro@quartile.co>
2021-07-20 11:17:26 +0800

    Add inventory valuate date field

---
+++ date_range_view_adj_hls/__manifest__.py
@@ -0,0 +1,14 @@
+# Copyright 2019 Quartile Limited
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+{
+    "name": "Date Range View Adj HLS",
+    "summary": "",
+    "version": "12.0.1.0.1",
+    "category": "Stock",
+    "website": "https://www.quartile.co/",
+    "author": "Quartile Limited",
+    "license": "AGPL-3",
+    "installable": True,
+    "depends": ["date_range"],
+    "data": ["views/date_range_views.xml"],
+}
---
+++ date_range_view_adj_hls/models/date_range.py
@@ -0,0 +1,12 @@
+# Copyright 2021 Quartile.co
+#   (http://www.quartile.co)
+
+from odoo import fields, models
+
+
+class DateRange(models.Model):
+    _inherit = "date.range"
+
+    inventory_valuate_date = fields.Date(
+        string="Inventory Valuate Date",
+    )
