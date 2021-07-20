6d9350981eca3f2b9c81178c61a40f14c2dd6c95
TomohiroAbe-Quartile <tomohiro@quartile.co>
2021-07-20 11:33:42 +0800

    rename module

--- inventory_valuate_date_hls/__manifest__.py
+++ inventory_valuate_date_hls/__manifest__.py
@@ -1,9 +1,9 @@
 # Copyright 2019 Quartile Limited
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 {
-    "name": "Date Range View Adj HLS",
+    "name": "Inventory Valuate Date HLS",
     "summary": "",
-    "version": "12.0.1.0.1",
+    "version": "12.0.1.0.0",
     "category": "Stock",
     "website": "https://www.quartile.co/",
     "author": "Quartile Limited",
