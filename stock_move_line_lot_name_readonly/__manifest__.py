cbf02596ac004a2193113675a2e091fd0a22e75a
TomohiroAbe-Quartile <tomohiro@quartile.co>
2021-07-29 16:01:00 +0800

    add new-module for uneditable field

---
+++ stock_move_line_lot_name_readonly/__manifest__.py
@@ -0,0 +1,13 @@
+# Copyright 2020 Quartile Limited
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
+{
+    "name": "Stock Move Line Removal Date",
+    "version": "12.0.1.0.1",
+    "author": "Quartile Limited",
+    "website": "https://www.quartile.co",
+    "category": "Stock",
+    "license": "LGPL-3",
+    "depends": ["product_expiry"],
+    "data": ["views/stock_move_line_views.xml"],
+    "installable": True,
+}
---
+++ stock_move_line_lot_name_readonly/readme/DESCRIPTION.rst
@@ -0,0 +1,3 @@
+This module does the following:
+
+* Allows user to input the Removal Date in Detailed Operations (stock move line) modal in receipts, which updates the corresponding field in generated lot/serial.
