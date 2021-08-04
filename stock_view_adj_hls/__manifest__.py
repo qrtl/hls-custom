cba4069e40f327689ca4deb4fd1134b0dfe1057d
TomohiroAbe-Quartile <tomohiro@quartile.co>
2021-08-04 14:35:50 +0800

    edit manifest and add move_line view

--- stock_view_adj_hls/__manifest__.py
+++ stock_view_adj_hls/__manifest__.py
@@ -10,5 +10,5 @@
     "license": "AGPL-3",
     "installable": True,
     "depends": ["product_expiry"],
-    "data": ["views/stock_quant_views.xml"],
+    "data": ["views/stock_quant_views.xml", "views/stock_move_line_views.xml"],
 }
