79e8bf7d1d33fd1dfeb022118798a2d758d76dc9
TomohiroAbe-Quartile <tomohiro@quartile.co>
2021-08-13 10:12:58 +0800

    add module

---
+++ stock_report_delivery_document_adj/__manifest__.py
@@ -0,0 +1,13 @@
+# Copyright 2021 Quartile Limited
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+{
+    "name": "Delivery Report Adjustment",
+    "version": "12.0.1.0.0",
+    "author": "Quartile Limited",
+    "website": "https://www.quartile.co",
+    "category": "Stock",
+    "license": "AGPL-3",
+    "depends": ["delivery", "stock"],
+    "data": ["report/report_deliveryslip.xml"],
+    "installable": True,
+}
---
+++ stock_report_delivery_document_adj/readme/DESCRIPTIOM.rst
@@ -0,0 +1,3 @@
+This module does the following:
+
+* Adjusts style the customer address and own address of delivery slip report.
