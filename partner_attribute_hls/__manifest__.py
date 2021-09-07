1fa897821f767e54e14106083c6dba21a997931e
TomohiroAbe-Quartile <84064473+TomohiroAbe-Quartile@users.noreply.github.com>
2021-09-07 14:20:05 +0900

    st2 [1803][IMP] partner_attribute_hls: add sql_constraint  unique (#149)
    Add sql constraint that customer_code to be unique field.

--- partner_attribute_hls/__manifest__.py
+++ partner_attribute_hls/__manifest__.py
@@ -2,7 +2,7 @@
 # License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
 {
     "name": "Partner Attribute HLS",
-    "version": "12.0.1.0.0",
+    "version": "12.0.1.0.1",
     "category": "Contact",
     "website": "https://www.quartile.co/",
     "author": "Quartile Limited",
