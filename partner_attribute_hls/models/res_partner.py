93c29795e60d00ced9c5eed862820975a0a34ce2
TomohiroAbe-Quartile <84064473+TomohiroAbe-Quartile@users.noreply.github.com>
2021-09-07 14:20:05 +0900

    st2 [1803][IMP] partner_attribute_hls: add sql_constraint  unique (#149)
    Add sql constraint that customer_code to be unique field.

--- partner_attribute_hls/models/res_partner.py
+++ partner_attribute_hls/models/res_partner.py
@@ -9,3 +9,11 @@ class ResPartner(models.Model):
     customer_code = fields.Char(
         copy=False, help="Intended to be passed to the external accounting system."
     )
+
+    _sql_constraints = [
+        (
+            "customer_code_uniq",
+            "UNIQUE(customer_code)",
+            "Customer Code must be unique.  Please enter another code.",
+        )
+    ]
