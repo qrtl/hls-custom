fcd9e1b300e44b3b1d5a38141fa4c5441e24888c
TomohiroAbe-Quartile <84064473+TomohiroAbe-Quartile@users.noreply.github.com>
2021-09-16 16:05:43 +0900

    st2 [ADD][1847] purchase_order_report_custom_hls:  (#150)
    Add representative sign and payment terms field in purchase order report.
    purchase_report_custom_hls

---
+++ purchase_report_custom_hls/__manifest__.py
@@ -0,0 +1,13 @@
+# Copyright 2021 Quartile Limited
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
+{
+    "name": "Purchase Order Report Custom",
+    "version": "12.0.1.0.0",
+    "category": "Purchase",
+    "author": "Quartile Limited",
+    "website": "https://www.quartile.co",
+    "license": "LGPL-3",
+    "installable": True,
+    "depends": ["purchase", "report_common_hls"],
+    "data": ["report/purchase_order_report_templates.xml"],
+}
---
+++ purchase_report_custom_hls/readme/DESCRIPTION.rst
@@ -0,0 +1,5 @@
+This module does the following:
+
+- Shows the payment terms and the signature image of the representative on the purchase order.
+
+The module depends on the report_common_hls module.
