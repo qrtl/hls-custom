9f236864be30adb4a4553a6e26c320eee11996dc
TomohiroAbe-Quartile <tomohiro@quartile.co>
2021-09-21 15:37:42 +0800

    pre-commit

--- inventory_valuate_date_hls/wizards/stock_quantity_history.py
+++ inventory_valuate_date_hls/wizards/stock_quantity_history.py
@@ -1,11 +1,7 @@
-import ast
 from odoo import fields, models
 
 
 class StockQuantityHistory(models.TransientModel):
     _inherit = "stock.quantity.history"
 
-    inventory_valuate_month = fields.Many2one(
-        "date.range",
-        readonly=True,
-    )
+    inventory_valuate_month = fields.Many2one("date.range", readonly=True,)
