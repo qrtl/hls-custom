1d4100f6836ba988346ff7ae784578d28e88ac57
TomohiroAbe-Quartile <tomohiro@quartile.co>
2021-09-29 15:00:46 +0800

    pre-commit

--- stock_quantity_history_date/wizards/stock_quantity_history.py
+++ stock_quantity_history_date/wizards/stock_quantity_history.py
@@ -1,4 +1,3 @@
-# -*- coding: utf-8 -*-
 # Copyright 2021 Quartile Limited
 # License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
 
@@ -6,10 +5,12 @@ from odoo import api, fields, models
 
 
 class StockQuantityHistory(models.TransientModel):
-    _inherit = 'stock.quantity.history'
+    _inherit = "stock.quantity.history"
 
     date_range_id = fields.Many2one("date.range", string="Date Range")
-    valuate_date = fields.Date(string="Inventory Valuate Date", compute="_compute_valuate_date")
+    valuate_date = fields.Date(
+        string="Inventory Valuate Date", compute="_compute_valuate_date"
+    )
 
     @api.onchange
     def _compute_valuate_date(self):
