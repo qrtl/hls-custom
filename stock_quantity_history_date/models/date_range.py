e8d9e67118c9c7d9746c942aaba0a4b637f85491
TomohiroAbe-Quartile <tomohiro@quartile.co>
2021-10-05 12:11:53 +0800

    edit translate, delete field string

--- stock_quantity_history_date/models/date_range.py
+++ stock_quantity_history_date/models/date_range.py
@@ -8,7 +8,6 @@ class DateRange(models.Model):
     _inherit = "date.range"
 
     valuation_date = fields.Datetime(
-        string="Inventory Valuate Date",
         help="Set the date that is intended to be used"
         "for showing the inventory at date.",
     )
