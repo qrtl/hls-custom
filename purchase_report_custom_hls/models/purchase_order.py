b22c6b037a5852c6547be9f37aace1f72ccf8909
TomohiroAbe-Quartile <tomohiro@quartile.co>
2021-10-13 12:06:00 +0800

    Add compute field

--- purchase_report_custom_hls/models/purchase_order.py
+++ purchase_report_custom_hls/models/purchase_order.py
@@ -1,4 +1,4 @@
-from odoo import fields, models
+from odoo import api, fields, models
 
 
 class PurchaseOrder(models.Model):
@@ -6,3 +6,13 @@ class PurchaseOrder(models.Model):
 
     estimated_time_arrival = fields.Char("Estimated Time of Arrival(ETA)")
     estimated_time_departure = fields.Char("Estimated Time of Departure(ETD)")
+    quantity_total = fields.Float(
+        "Total Quantity", compute="_compute_quantity_total"
+    )
+
+    @api.onchange("order_line")
+    def _compute_quantity_total(self):
+        qty_total = 0
+        for pol in self.order_line:
+            qty_total += pol.product_qty
+        self.quantity_total = qty_total
