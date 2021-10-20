2ef9a42b9e87af639ee847a89e3540869dcb950f
TomohiroAbe-Quartile <tomohiro@quartile.co>
2021-10-20 12:31:41 +0800

    add t-if and bool field

--- purchase_report_custom_hls/models/purchase_order.py
+++ purchase_report_custom_hls/models/purchase_order.py
@@ -7,6 +7,16 @@ class PurchaseOrder(models.Model):
     eta_date = fields.Char("ETA")
     etd_date = fields.Char("ETD")
     quantity_total = fields.Float("Total Quantity", compute="_compute_quantity_total")
+    display_tax = fields.Boolean(
+        "Display Tax",
+        default=False,
+        help="If checked, display tax in purchase order report.",
+    )
+    display_date_req = fields.Boolean(
+        "Display Requested Date",
+        default=False,
+        help="If checked, display requested date in purchase order report.",
+    )
 
     @api.depends("order_line")
     def _compute_quantity_total(self):
