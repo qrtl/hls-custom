33dae9657ce5cacece7b3b6db4522258a20e7646
TomohiroAbe-Quartile <tomohiro@quartile.co>
2021-11-17 09:18:35 +0800

    Edit logic

--- account_invoice_line_salesperson/models/account_invoice_line.py
+++ account_invoice_line_salesperson/models/account_invoice_line.py
@@ -7,17 +7,14 @@ from odoo import api, fields, models
 class AccountInvoiceLine(models.Model):
     _inherit = "account.invoice.line"
 
-    user_id = fields.Many2one(
-        "res.users", "Sales Person", readonly=True, compute="_compute_user_id",
+    salesman_id = fields.Many2one(
+        "res.users", "Sales Person", compute="_compute_salesman_id", store=True
     )
 
-    @api.multi
-    def _compute_user_id(self):
+    @api.depends("sale_line_ids")
+    def _compute_salesman_id(self):
         for line in self:
-            order = self.env["sale.order"].search([("name", "=", self.sale_order_name)])
-            if order:
-                user = self.env["res.users"].search([("id", "=", order.user_id.id)])
-        if user:
-            line.user_id = user
-        else:
-            line.user_id = line.create_uid
+            if line.sale_line_ids:
+                line.salesman_id = line.sale_line_ids[0].salesman_id
+            else:
+                line.salesman_id = line.invoice_id.user_id
