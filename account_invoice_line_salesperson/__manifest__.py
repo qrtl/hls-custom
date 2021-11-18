78ee3f4e8aebdd4024379960a626308d3dee0d73
TomohiroAbe-Quartile <84064473+TomohiroAbe-Quartile@users.noreply.github.com>
2021-11-18 11:45:42 +0900

    st2 [2044][ADD] account_invoice_line_salesperson (#160)
    2044 Add salesman_id field in account_invoice_line model.
    
    need to install/upgrade
    
    account_invoice_line_salesperson
    account_view_adj_hls

---
+++ account_invoice_line_salesperson/__manifest__.py
@@ -0,0 +1,12 @@
+# Copyright 2021 Quartile Limited
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+{
+    "name": "Salesperson in Account Invoice Line",
+    "version": "12.0.1.0.0",
+    "author": "Quartile Limited",
+    "website": "https://www.quartile.co",
+    "category": "Accounting",
+    "license": "AGPL-3",
+    "depends": ["sale"],
+    "installable": True,
+}
---
+++ account_invoice_line_salesperson/readme/DESCRIPTION.rst
@@ -0,0 +1,3 @@
+This module adds salesperson in account invoice line.
+* The salesperson value is taken from the linked sales order line if any.  Otherwise, the value is taken from user_id of the invoice.
+The salesperson field is expected to be used in the invoice analysis report which is prepared with SQL View.
