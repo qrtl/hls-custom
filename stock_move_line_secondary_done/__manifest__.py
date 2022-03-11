3003afc4cb4b89f14f83c62fa43235fd8cfbcc5b
Yoshi Tashiro (Quartile) <tashiro@quartile.co>
2022-03-11 22:48:07 +0900

    [2195][ADD] stock_move_line_secondary_done (#173)

---
+++ stock_move_line_secondary_done/__manifest__.py
@@ -0,0 +1,13 @@
+# Copyright 2022 Quartile Limited
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+{
+    "name": "Stock Move Line Secondary Done",
+    "version": "12.0.1.0.0",
+    "author": "Quartile Limited",
+    "website": "https://www.quartile.co",
+    "category": "Stock",
+    "license": "AGPL-3",
+    "depends": ["stock_secondary_unit"],
+    "data": ["views/stock_move_line_views.xml"],
+    "installable": True,
+}
