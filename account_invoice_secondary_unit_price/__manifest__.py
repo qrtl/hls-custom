b6dcfee7103924059908850fefe98de2f32e3959
kanda999 <101688144+kanda999@users.noreply.github.com>
2022-08-15 17:57:25 +0900

    st2 [2663][IMP] sale_order_secondary_unit_price account_invoice_secondary_unit_price (#181)
    add constrain if no seccondary uom entered but unit price is entered

--- account_invoice_secondary_unit_price/__manifest__.py
+++ account_invoice_secondary_unit_price/__manifest__.py
@@ -2,7 +2,7 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 {
     "name": "Account Invoice Secondary UoM Price",
-    "version": "12.0.1.0.0",
+    "version": "12.0.1.1.0",
     "author": "Quartile Limited",
     "website": "https://www.quartile.co",
     "category": "Accounting",
