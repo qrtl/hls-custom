7fc21cc8e1c2d7dfb803e32303286378724b6f71
AungKoKoLin <45355704+AungKoKoLin1997@users.noreply.github.com>
2022-10-21 08:32:23 +0630

    st2 [2808][ADD] stock_return_picking_refund (#193)
    * [ADD] stock_picking_reverse_auto_check_refund

---
+++ stock_return_picking_refund/__manifest__.py
@@ -0,0 +1,12 @@
+# Copyright 2019-2020 Quartile Limited
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+{
+    "name": "Stock Return Picking Refund",
+    "version": "12.0.1.0.0",
+    "category": "Stock Account",
+    "website": "https://www.quartile.co",
+    "author": "Quartile Limited",
+    "license": "AGPL-3",
+    "installable": True,
+    "depends": ["stock_account"],
+}
---
+++ stock_return_picking_refund/readme/DESCRIPTION.rst
@@ -0,0 +1,3 @@
+This module does the following:
+
+- Default check "To Refund (update SO/PO)" in the reverse transfer of inventory transfer.
