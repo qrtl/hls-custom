18f8c3bc102616a61c0c24ff5269bdfe29c14337
kanda999 <kanda@quartile.co>
2022-11-18 10:24:05 +0800

    [ADD] account_invoice_line_salesperson

---
+++ account_invoice_line_salesperson/__manifest__.py
@@ -0,0 +1,13 @@
+# Copyright 2021 Quartile Limited
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+{
+    "name": "Salesperson in Account Invoice Line",
+    "version": "12.0.1.0.0",
+    "author": "Quartile Limited",
+    "website": "https://www.quartile.co",
+    "category": "Accounting",
+    "license": "AGPL-3",
+    "depends": ["sale"],
+    "data": ["views/account_invoice_views.xml"],
+    "installable": True,
+}
---
+++ account_invoice_line_salesperson/models/account_invoice_line.py
@@ -0,0 +1,24 @@
+# Copyright 2021 Quartile Limited
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from odoo import api, fields, models
+
+
+class AccountInvoiceLine(models.Model):
+    _inherit = "account.invoice.line"
+
+    salesman_id = fields.Many2one(
+        "res.users",
+        "Sales Person",
+        readonly=True,
+        compute="_compute_salesman_id",
+        store=True,
+    )
+
+    @api.depends("sale_line_ids")
+    def _compute_salesman_id(self):
+        for line in self:
+            if line.sale_line_ids:
+                line.salesman_id = line.sale_line_ids[0].salesman_id
+            else:
+                line.salesman_id = line.invoice_id.user_id
