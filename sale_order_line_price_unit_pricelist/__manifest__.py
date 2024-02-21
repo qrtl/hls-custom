028824096d269bf9e3a3e870edd9d46ddff000d3
Aungkokolin1997 <aungkokolin23397@gmail.com>
2024-02-21 13:33:58 +0800

    [ADD] sale_order_line_price_unit_pricelist

---
+++ sale_order_line_price_unit_pricelist/__manifest__.py
@@ -0,0 +1,13 @@
+# Copyright 2024 Quartile Limited
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+{
+    "name": "Sale Order Line Price Unit Pricelist",
+    "version": "12.0.1.0.0",
+    "author": "Quartile Limited",
+    "website": "https://www.quartile.co",
+    "category": "Sales",
+    "license": "AGPL-3",
+    "depends": ["sale"],
+    "data": ["views/sale_order_views.xml"],
+    "installable": True,
+}
