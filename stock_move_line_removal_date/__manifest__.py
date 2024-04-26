46023f82466d4368b1d313149a0b2b53dc2bce9c
kanda999 <kanda@quartile.co>
2024-04-26 14:56:23 +0800

    [IMP] stock_move_line_removal_date

--- stock_move_line_removal_date/__manifest__.py
+++ stock_move_line_removal_date/__manifest__.py
@@ -2,12 +2,12 @@
 # License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
 {
     "name": "Stock Move Line Removal Date",
-    "version": "12.0.1.0.1",
+    "version": "12.0.1.1.0",
     "author": "Quartile Limited",
     "website": "https://www.quartile.co",
     "category": "Stock",
     "license": "LGPL-3",
     "depends": ["product_expiry"],
-    "data": ["views/stock_move_line_views.xml"],
+    "data": ["views/stock_move_line_views.xml", "views/stock_picking_views.xml"],
     "installable": True,
 }
