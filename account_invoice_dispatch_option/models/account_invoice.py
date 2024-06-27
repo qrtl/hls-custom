94495254399c365af7a62149becbd5435d68d708
Aung Ko Ko Lin (Quartile) <45355704+AungKoKoLin1997@users.noreply.github.com>
2024-06-27 16:30:25 +0700

    [4332][ADD] account_invoice_dispatch_option (#223)
    [ADD] account_invoice_dispatch_option (#223)

---
+++ account_invoice_dispatch_option/models/account_invoice.py
@@ -0,0 +1,32 @@
+# Copyright 2024 Quartile
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
+from odoo import api, fields, models
+
+
+class AccountInvoice(models.Model):
+    _inherit = "account.invoice"
+
+    use_company_invoice = fields.Boolean()
+    invoice_send_method = fields.Selection(
+        [
+            ("post", "Post"),
+            ("email", "Email"),
+            ("do_not_send", "Do Not Send"),
+            ("others", "Others"),
+        ],
+    )
+
+    @api.onchange("partner_id", "company_id")
+    def _onchange_partner_id(self):
+        self.use_company_invoice = self.partner_id.use_company_invoice
+        self.invoice_send_method = self.partner_id.invoice_send_method
+        return super()._onchange_partner_id()
+
+    def _get_onchange_create(self):
+        onchanges = super()._get_onchange_create()
+        if "_onchange_partner_id" in onchanges:
+            onchanges["_onchange_partner_id"].extend(
+                ["use_company_invoice", "invoice_send_method"]
+            )
+        return onchanges
