b20954219148afea5899ab59d3529e28cb9b88f1
kanda999 <kanda@quartile.co>
2024-08-06 18:12:38 +0800

    [ADD] stock_report_deliveryslip_adj

---
+++ stock_report_deliveryslip_adj/__manifest__.py
@@ -0,0 +1,19 @@
+# Copyright 2024 Quartile
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+{
+    "name": "Stock Report Delivery Slip Adjustments",
+    "version": "12.0.1.0.0",
+    "category": "Reporting",
+    "license": "AGPL-3",
+    "author": "Quartile",
+    "website": "https://www.quartile.co",
+    "depends": [
+        "sale_stock",
+        "sale_order_line_customer_reference",
+        "stock_picking_report_valued",
+    ],
+    "data": [
+        "report/report_deliveryslip.xml",
+    ],
+    "installable": True,
+}
