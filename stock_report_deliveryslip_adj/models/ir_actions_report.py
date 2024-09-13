6136335aa2eecda998f0ff02da48483d4f5b491b
Aungkokolin1997 <aungkokolin23397@gmail.com>
2024-09-13 15:57:12 +0800

    run pre-commit, upd

--- stock_report_deliveryslip_adj/models/ir_actions_report.py
+++ stock_report_deliveryslip_adj/models/ir_actions_report.py
@@ -1,16 +1,17 @@
 # Copyright 2024 Quartile Limited
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from odoo import models, fields
+from odoo import fields, models
+
 
 class Report(models.Model):
-    _inherit = 'ir.actions.report'
+    _inherit = "ir.actions.report"
 
     commercial_partner = fields.Boolean()
 
-    def _get_partner_id(self, doc):
+    def _get_partner_id(self, xmlid, doc):
         doc.ensure_one()
-        # report = self._get_report_from_name("stock.report_deliveryslip")
+        report = self._get_report_from_name(xmlid)
         if report.commercial_partner:
             return doc.partner_id.commercial_partner_id
         else:
