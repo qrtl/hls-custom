1acfff2b5f9be69e5b83348db02102319c908b09
Tatsuki Kanda (Quartile) <101688144+kanda999@users.noreply.github.com>
2025-01-06 14:36:02 +0900

    [4419][ADD] stock_report_deliveryslip_adj, report_alternative_layout (#227)
    [4419][ADD] stock_report_deliveryslip_adj, report_alternative_layout (#227)

---
+++ report_alternative_layout/models/ir_actions_report.py
@@ -0,0 +1,29 @@
+# Copyright 2024 Quartile
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from odoo import api, fields, models
+
+
+class Report(models.Model):
+    _inherit = "ir.actions.report"
+
+    apply_alternative_layout = fields.Boolean(
+        help="If selected, the alternative layout will be applied in the printed "
+        "report.",
+    )
+    show_commercial_partner = fields.Boolean(
+        help="If selected, the commercial partner of the document partner will show "
+        "in the report output (instead of the document partner)."
+    )
+
+    @api.multi
+    def render_qweb_pdf(self, res_ids=None, data=None):
+        if self.apply_alternative_layout:
+            self = self.with_context(apply_alternative_layout=True)
+        return super().render_qweb_pdf(res_ids, data)
+
+    def _get_partner(self, xmlid, partner):
+        report = self._get_report_from_name(xmlid)
+        if report.show_commercial_partner:
+            return partner.commercial_partner_id
+        return partner
---
+++ stock_report_deliveryslip_adj/__manifest__.py
@@ -0,0 +1,22 @@
+# Copyright 2024 Quartile
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+{
+    "name": "Stock Report Delivery Slip Adjustments",
+    "version": "12.0.1.0.0",
+    "category": "Reporting",
+    "license": "AGPL-3",
+    "author": "Quartile",
+    "website": "https://www.quartile.co",
+    "depends": [
+        "sale_stock",
+        "sale_order_delivery_date",
+        "sale_order_line_customer_reference",
+        "stock_picking_report_valued",
+        "stock_secondary_unit",
+    ],
+    "data": [
+        "data/report_deliveryslip_data.xml",
+        "report/report_deliveryslip.xml",
+    ],
+    "installable": True,
+}
