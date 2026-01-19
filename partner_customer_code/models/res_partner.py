a161f6917b22a91841d2292e7263421add2240d5
Aungkokolin1997 <lin@quartile.co>
2026-01-19 02:30:29 +0000

    [MIG] partner_customer_code: Migration to 18.0

--- partner_customer_code/models/res_partner.py
+++ partner_customer_code/models/res_partner.py
@@ -1,4 +1,5 @@
-# Copyright 2021 Quartile Limited
+# Copyright 2021 Quartile (https://www.quartile.co)
+# License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl).
 
 from odoo import fields, models
 
