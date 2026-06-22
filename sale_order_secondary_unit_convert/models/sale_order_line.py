cab23f45f4765abc0e21979902d38145fa4d77ef
Morita Shinnosuke <morita@quartile.co>
2026-06-22 06:44:15 +0000

    fixup!

--- sale_order_secondary_unit_convert/models/sale_order_line.py
+++ sale_order_secondary_unit_convert/models/sale_order_line.py
@@ -16,9 +16,23 @@ class SaleOrderLine(models.Model):
         string="Input Qty",
         digits="Product Unit of Measure",
         compute="_compute_input_secondary_qty",
+        inverse="_inverse_input_secondary_qty",
         store=True,
         readonly=False,
     )
+
+    def _convert_qty_to_input_secondary_uom(self):
+        self.ensure_one()
+        uom = self.input_secondary_uom_id
+        qty = self.product_uom_qty
+        uom_line = self.product_uom
+        uom_product = self.product_id.uom_id
+        if uom_line and uom_product and uom_line != uom_product:
+            qty = uom_line._compute_quantity(qty, uom_product)
+        return float_round(
+            qty / (uom.factor or 1.0),
+            precision_rounding=uom.uom_id.rounding,
+        )
 
     @api.depends(
         "product_id", "product_uom_qty", "product_uom", "input_secondary_uom_id"
@@ -29,27 +43,15 @@ class SaleOrderLine(models.Model):
             if not uom or uom.dependency_type == "independent":
                 line.input_secondary_qty = 0.0
                 continue
-            line.input_secondary_qty = float_round(
-                line._secondary_base_qty() / (uom.factor or 1.0),
-                precision_rounding=uom.uom_id.rounding,
+            line.input_secondary_qty = line._convert_qty_to_input_secondary_uom()
+
+    def _inverse_input_secondary_qty(self):
+        for line in self:
+            uom = line.input_secondary_uom_id
+            if not uom or uom.dependency_type == "independent" or not line.product_id:
+                continue
+            base_qty = line.input_secondary_qty * (uom.factor or 1.0)
+            line.product_uom_qty = line.product_id.uom_id._compute_quantity(
+                base_qty, line.product_uom
             )
-
-    def _secondary_base_qty(self):
-        self.ensure_one()
-        qty = self.product_uom_qty
-        uom_line = self.product_uom
-        uom_product = self.product_id.uom_id
-        if uom_line and uom_product and uom_line != uom_product:
-            qty = uom_line._compute_quantity(qty, uom_product)
-        return qty
-
-    @api.onchange("input_secondary_qty")
-    def _onchange_input_secondary(self):
-        uom = self.input_secondary_uom_id
-        if not uom or uom.dependency_type == "independent" or not self.product_id:
-            return
-        base_qty = self.input_secondary_qty * (uom.factor or 1.0)
-        self.product_uom_qty = self.product_id.uom_id._compute_quantity(
-            base_qty, self.product_uom
-        )
-        self.env.remove_to_compute(self._fields["input_secondary_qty"], self)
+            line.env.remove_to_compute(line._fields["input_secondary_qty"], line)
