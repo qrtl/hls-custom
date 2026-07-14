4a01f9b7d3697f55a5a81b15b49ee5206733b6ed
Morita Shinnosuke <morita@quartile.co>
2026-07-14 07:59:52 +0000

    [7006][ADD] stock_inventory_hide_apply_all
    Add a module that hides the 'Apply All' button on the inventory adjustment
    list so that counted quantities are applied and reviewed line by line instead
    of committing them in bulk.

---
+++ stock_inventory_hide_apply_all/__manifest__.py
@@ -0,0 +1,14 @@
+# Copyright 2026 Quartile (https://www.quartile.co)
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+{
+    "name": "Stock Inventory Hide Apply All",
+    "summary": "Hide the 'Apply All' button on the inventory adjustment list",
+    "version": "18.0.1.0.0",
+    "author": "Quartile",
+    "website": "https://www.quartile.co",
+    "category": "Warehouse Management",
+    "license": "AGPL-3",
+    "depends": ["stock"],
+    "data": ["views/stock_quant_views.xml"],
+    "installable": True,
+}
---
+++ stock_inventory_hide_apply_all/readme/DESCRIPTION.md
@@ -0,0 +1,1 @@
+This module hides the **Apply All** button on the inventory adjustment list.
