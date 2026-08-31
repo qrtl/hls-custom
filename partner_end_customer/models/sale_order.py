5caf570e08a5958efb28249ef54f39bdc3da8c57
Morita Shinnosuke <morita@quartile.co>
2026-08-31 10:41:20 +0000

    fixup!

--- partner_end_customer/models/sale_order.py
+++ partner_end_customer/models/sale_order.py
@@ -17,3 +17,8 @@ class SaleOrder(models.Model):
     def _compute_end_customer(self):
         for order in self:
             order.end_customer = order.partner_id.end_customer
+
+    def _prepare_invoice(self):
+        values = super()._prepare_invoice()
+        values["end_customer"] = self.end_customer
+        return values
